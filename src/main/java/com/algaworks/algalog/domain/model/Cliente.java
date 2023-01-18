aa034dcb412e8519bf9161671fd1da5ffd9cccd6
Marcos Giovanny <giovannygigi@gmail.com>
2023-01-18 19:42:45 -0300

    Adicionado e testado o Jakarta Persistence(JPA)

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,44 +1,89 @@
 package com.algaworks.algalog.domain.model;
 
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Cliente {
 	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long Id;
+	
+	
 	private String nome;
+	
 	private String email;
+	
+	@Column(name = "fone")
 	private String telefone;
 	
 	
 	
-	
+
 	public Long getId() {
 		return Id;
 	}
+	
 	public void setId(Long id) {
 		Id = id;
 	}
+	
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
 	public String getTelefone() {
 		return telefone;
 	}
+	
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(Id, other.Id);
+	}
+	
+	
 	
 
 }
