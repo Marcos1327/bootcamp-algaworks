1ce585c8591ceee9f049024e77cbbecfed3bc795
Marcos Giovanny <giovannygigi@gmail.com>
2023-01-20 17:54:52 -0300

    Isolando Domain Model do Representation Model

---
+++ src/main/java/com/algaworks/algalog/domain/model/EntregaModel.java
@@ -0,0 +1,20 @@
+package com.algaworks.algalog.domain.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EntregaModel {
+	
+	private Long id;
+	private String nomeCliente;
+	private DestinatarioModel destinatario;
+	private BigDecimal taxa;
+	private OffsetDateTime dataPedido;
+	private OffsetDateTime dataFinalizacao;
+
+}
